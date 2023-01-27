ccb5902e119029210a4371ef4f790606dddf9ac9
Maikon Fernando da Silva <55884605+m4ik-crtl@users.noreply.github.com>
2023-01-26 22:10:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let teacherForms = document.getElementsByClassName("teacher-contract")
+for (let teacherForm of teacherForms){
+    teacherForm.addEventListener("submit", function (event) {
+        event.preventDefault()
+        console.log("executou o submit")
+
+        let price = teacherForm.price.value
+        let hours = teacherForm.hours.value
+        let total = price + hours
+
+        console.log(total)
+        alert("O valor total do contrato é de R$ " + total)
+    }) 
+}
